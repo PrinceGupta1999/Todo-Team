bf58162622516df84b742972c84421424f92964d
Prince Gupta <princeg.co.17@nsit.net.in>
2019-05-12 16:22:46 +0530

    Update Dependencies and Rearrange Structure

---
+++ models/Todo.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const Todo = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    responsible: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+});
+
+module.exports = Todo = mongoose.model("Todo", TodoListSchema);
