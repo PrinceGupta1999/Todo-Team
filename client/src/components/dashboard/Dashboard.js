fb992cb25f562e9fde8f6361e862ed6fe20c43e3
Prince Gupta <princeg.co.17@nsit.net.in>
2019-05-15 20:26:56 +0530

    Complete Navbar Component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import Navbar from "../layout/Navbar";
 
 class Dashboard extends Component {
     onLogoutClick = e => {
@@ -13,30 +14,8 @@ class Dashboard extends Component {
         const { user } = this.props.auth;
 
         return (
-            <div style={{ height: "75vh" }} className="container valign-wrapper">
-                <div className="row">
-                    <div className="landing-copy col s12 center-align">
-                        <h4>
-                            <b>Hey there,</b> {user.name.split(" ")[0]}
-                            <p className="flow-text grey-text text-darken-1">
-                                You are logged into {" "}
-                                <span style={{ fontFamily: "monospace" }}>TODO Team</span> app 👏
-              </p>
-                        </h4>
-                        <button
-                            style={{
-                                width: "150px",
-                                borderRadius: "3px",
-                                letterSpacing: "1.5px",
-                                marginTop: "1rem"
-                            }}
-                            onClick={this.onLogoutClick}
-                            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                        >
-                            Logout
-            </button>
-                    </div>
-                </div>
+            <div>
+                <Navbar />
             </div>
         );
     }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,121 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { connect } from "react-redux";
+import PropTypes from 'prop-types';
+import { logoutUser } from "../../actions/authActions";
+import { clearErrors } from "../../actions/errorActions";
+import { withStyles } from '@material-ui/core/styles';
+import {
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+    Badge,
+    Menu,
+    MenuItem,
+    Avatar,
+    Fab,
+} from '@material-ui/core';
+import deepOrange from "@material-ui/core/colors/deepOrange"
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
-class Navbar extends Component {
+const styles = theme => ({
+    root: {
+        width: '100%',
+    },
+    title: {
+        flexGrow: 1,
+    },
+    button: {
+        marginRight: theme.spacing.unit * 2
+    },
+    avatar: {
+        backgroundColor: deepOrange[500]
+    },
+});
+
+class Navbar extends React.Component {
+    state = {
+        accountAnchor: null,
+        notificationAnchor: null
+    }
+    getNameInitials = name => {
+        var initials = name.match(/\b\w/g) || [];
+        initials = ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
+        return initials
+    }
+    handleOpen = name => e => {
+        this.setState({
+            [name + 'Anchor']: e.currentTarget
+        })
+        console.log(this.state)
+    }
+    handleClose = name => e => {
+        this.setState({
+            [name + 'Anchor']: null
+        })
+    }
     render() {
+        const { classes } = this.props;
+        const { notificationAnchor, accountAnchor } = this.state;
+        const { user } = this.props.auth;
         return (
-            <div className="navbar-fixed">
-                <nav className="z-depth-0">
-                    <div className="nav-wrapper white">
-                        <Link
-                            to="/"
-                            style={{
-                                fontFamily: "monospace"
-                            }}
-                            className="col s5 brand-logo center black-text"
-                        >
-                            <i>TODO TEAM</i>
-                        </Link>
-                    </div>
-                </nav>
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography className={classes.title} variant="h6" color="inherit">
+                            TODO Team
+                            </Typography>
+                        <IconButton
+                            className={classes.button}
+                            color="inherit"
+                            onClick={this.handleOpen('notification')}>
+                            <Badge badgeContent={11} color="secondary">
+                                <NotificationsIcon />
+                            </Badge>
+                        </IconButton>
+                        <Menu
+                            anchorEl={notificationAnchor}
+                            open={Boolean(notificationAnchor)}
+                            onClose={this.handleClose('notification')}>
+                            <MenuItem onClick={this.handleClose('notification')}>My account</MenuItem>
+                            <MenuItem onClick={this.handleClose('notification')}>Logout</MenuItem>
+                            <MenuItem onClick={this.handleClose('notification')}>Profile</MenuItem>
+                        </Menu>
+                        <Fab
+                            className={classes.button}
+                            onClick={this.handleOpen('account')}
+                            size="small">
+                            <Avatar className={classes.avatar}>{this.getNameInitials(user.name)}</Avatar>
+                        </Fab>
+                        <Menu
+                            anchorEl={accountAnchor}
+                            open={Boolean(accountAnchor)}
+                            onClose={this.handleClose('account')}>
+                            <MenuItem onClick={this.handleClose('account')}>Hi! {user.name}</MenuItem>
+                            <MenuItem onClick={() => this.props.logoutUser()}>Logout</MenuItem>
+                        </Menu>
+
+                    </Toolbar>
+                </AppBar>
             </div>
         );
     }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+    classes: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+})
+export default connect(
+    mapStateToProps,
+    {
+        logoutUser,
+        clearErrors
+    }
+)(withStyles(styles)(Navbar));
